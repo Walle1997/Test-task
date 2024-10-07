1b346333beff986d7e720e4e13a0c75daa04bd0e
Walle1997 <farid-keltuzed@mail.ru>
2024-10-07 18:48:56 +0300

    .

---
+++ test2.js
@@ -0,0 +1,19 @@
+function findCommons(arr) {
+    if (!arr.length) {
+        return [];
+     };
+
+    const minNumber = Math.min(...arr);
+    const commonElements = [];
+
+    for (let i = 1; i <= minNumber; i++) {
+        if (minNumber % i === 0) {
+            if (arr.every(num => num % i === 0)) {
+                commonElements.push(i);
+            }
+        }
+    }
+    return commonElements;
+};
+
+findCommons(arr);
